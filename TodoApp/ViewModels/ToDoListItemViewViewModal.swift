55bb6da5b52716fb1a694bad5c5c91e2c30f3141
rpek99 <rstmpk28@gmail.com>
2024-03-01 01:20:16 +0300

    project completed

--- TodoApp/ViewModels/ToDoListItemViewViewModal.swift
+++ TodoApp/ViewModels/ToDoListItemViewViewModal.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 // view model for single todo list item view
 class ToDoListItemViewViewModal: ObservableObject{
@@ -14,6 +16,19 @@ class ToDoListItemViewViewModal: ObservableObject{
     }
     
     func toggleIsDone(item: ToDoListItem) {
+        var existItem = item
+        existItem.setDone(!item.isDone)
         
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(existItem.id)
+            .setData(existItem.asDictionary())
+
     }
 }
--- TodoApp/Views/ProfileView.swift
+++ TodoApp/Views/ProfileView.swift
@@ -13,9 +13,61 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack{
-                
-            }.navigationTitle("Profile")
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading profile...")
+                }
+            }
+            .navigationTitle("Profile")
         }
+        .onAppear {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        // Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // User infos
+        VStack(alignment: .leading){
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+            
+        }
+        .padding()
+        
+        // Sign out button
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .foregroundColor(.red)
+        .padding()
     }
 }
 
